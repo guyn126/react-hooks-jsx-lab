729ee4cf072e0efe032607fd798e6cd7ff91ba7b
student.moringaschool.com <guy.nkurunziza@student.moringaschool.com>
2025-04-17 09:48:57 +0200

    jsx_lab

--- src/components/About.js
+++ src/components/About.js
@@ -2,7 +2,12 @@ import React from "react";
 import { image } from "../data/data";
 
 function About() {
-  return <div>About</div>;
+  return( <div id="about">
+    <h2>About Me</h2>
+    <p>I love building creative, user-friendly web applications that solve real-word problems.</p>
+    <img src={image} alt="I made this" />
+  </div>
+  );
 }
 
 export default About;
